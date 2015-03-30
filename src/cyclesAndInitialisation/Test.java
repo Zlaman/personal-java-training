de52121b3daf354d4d230e3a33c6d5cd3fe2be0a
ozaika <ozaika@carbonite.com>
2015-03-30 23:18:23 +0300

    Update 2 classes

--- src/cyclesAndInitialisation/Test.java
+++ src/cyclesAndInitialisation/Test.java
@@ -2,9 +2,6 @@ package cyclesAndInitialisation;
 
 import org.jetbrains.annotations.*;
 
-/**
- * Created by oleksandr.zaika on 3/18/2015.
- */
 public class Test {
     public static void main(String[] args) {
         for (int i = 0; i < 100; i++) {
