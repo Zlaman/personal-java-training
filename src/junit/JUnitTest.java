984f854bc47990f78faa1f1fc65165925433dbf8
ozaika <ozaika@carbonite.com>
2015-10-12 18:00:06 +0300

    Update class

--- src/junit/JUnitTest.java
+++ src/junit/JUnitTest.java
@@ -11,7 +11,7 @@ import org.junit.experimental.theories.Theory;
 import org.junit.runner.RunWith;
 
 @RunWith(Theories.class)
-public class JunitTest {
+public class JUnitTest {
 
         @DataPoints
         public static Boolean A1 = true;
